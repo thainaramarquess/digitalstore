b68247de37185bca41c602047785b5a63c087047
Thainara Marques da Costa <79341428+thainaramarquess@users.noreply.github.com>
2025-01-06 14:41:32 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './app.css'
+import './App.css'
 
 const root = createRoot(document.getElementById('root'))
 root.render(
